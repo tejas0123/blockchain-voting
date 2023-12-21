35096c1bd653d1f7fc0e82fb3d7b926e650d2bcb
prathibhabilagi <prathibhabilagi12@gmail.com>
2023-12-21 21:31:28 +0530

    init setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Main from './Components/Main.js';
+
+function App() {
+  return (
+      <div className="App">
+        <Main/>
+      </div>
+
+  );
+}
+
+export default App;
---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+  return (
+    <div>
+        <p>
+          Blockchain voting system 
+        </p>
+    </div>
+  );
+}
+
+export default Main;
